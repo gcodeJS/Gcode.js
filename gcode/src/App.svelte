7f5ed9d589028057db0106eef9eb0fde8d61c261
laaouatni <laaouatni.anas@outlook.it>
2022-12-03 18:50:26 +0100

    colored gcode

--- gcode/src/App.svelte
+++ gcode/src/App.svelte
@@ -7,7 +7,7 @@
 
   function createRandomArray() {
     array = [];
-    const maxY = (window.innerHeight / 100 * 40)
+    const maxY = (window.innerHeight / 100) * 40;
     for (let i = 0; i <= 100; i++) {
       array = [
         ...array,
@@ -21,12 +21,15 @@
 </script>
 
 <main
-  class="flex flex-wrap justify-center items-center gap-10 h-full overflow-auto p-10"
+  class="p-5 h-full overflow-auto"
 >
   <button
-    class="bg-sky-500 p-4 rounded-lg text-2xl text-white hover:shadow-lg hover:scale-110 focus:outline hover:shadow-sky-600/30 transition"
+    class="bg-sky-500 p-4 mb-4 rounded-lg text-2xl text-white hover:shadow-lg hover:scale-110 focus:outline hover:shadow-sky-600/30 transition"
     on:click={createRandomArray}>click me</button
   >
-  <Gcanvas coordsArray={array} anim_speed={ANIMATION_SPEED} />
-  <Gsource coordsArray={array} anim_speed={ANIMATION_SPEED} />
+
+  <div class="flex flex-wrap gap-5 justify-center">
+    <Gcanvas coordsArray={array} anim_speed={ANIMATION_SPEED} />
+    <Gsource coordsArray={array} />
+  </div>
 </main>
--- gcode/src/lib/GlineCode.svelte
+++ gcode/src/lib/GlineCode.svelte
@@ -1,46 +1,49 @@
 <script>
   export let positionsObj;
   export let index;
-
-  // function scrollToThisElement(e) {
-  //   e.target.scrollIntoView();
-  // }
 </script>
 
-<div
-  class="flex gap-2"
->
-  <span>N{index}</span>
-  <span>G1</span>
+<div class="flex gap-2">
+  <span class="text-slate-500">N{index}</span>
+  <span class="text-violet-500 font-bold">G1</span>
 
   <div class="flex gap-2">
     {#each Object.entries(positionsObj) as positions}
       <div class="flex">
         {#each positions as pos, index}
-          <span>
-            {#if index === 0}
+          {#if index === 0}
+            <span class="text-cyan-500 font-bold">
               {pos.toUpperCase()}
-            {:else}
+            </span>
+          {:else}
+            <span class="text-rose-500">
               {pos.toFixed(2)}
-            {/if}
-          </span>
+            </span>
+          {/if}
         {/each}
       </div>
     {/each}
   </div>
 </div>
 
-<!-- <style>
-  div {
-    animation: show var(--line-delay);
+<style>
+  .G-command-name {
+    color: rgb(150, 0, 255);
   }
 
-  @keyframes show {
-    from {
-      scale: 0;
-    }
-    to {
-      scale: 1;
-    }
+  .G-option-coords {
+    color: rgb(100, 150, 255);
   }
-</style> -->
+
+  .G-options-M {
+    color: rgb(0, 255, 150);
+  }
+
+  .G-number-value {
+    color: rgb(255, 100, 50);
+  }
+
+  .G-comment {
+    color: rgb(150, 150, 150);
+  }
+</style>
