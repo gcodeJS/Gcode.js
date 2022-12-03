9b0157806f9ef03e190c1f1906c6b9a91e3232c0
laaouatni <laaouatni.anas@outlook.it>
2022-12-03 18:15:56 +0100

    no animation on lines of code

--- gcode/src/lib/Gsource.svelte
+++ gcode/src/lib/Gsource.svelte
@@ -2,14 +2,15 @@
   import GlineCode from "./GlineCode.svelte";
 
   export let coordsArray;
-  export let anim_speed = 20;
 </script>
 
 {#if coordsArray.length > 0}
-<div class="px-10 py-4 bg-white/80 shadow-lg rounded-lg max-h-[100%] overflow-auto">
-  <h1 class="text-3xl font-extrabold text-slate-400 mb-4">Source Code:</h1>
-    {#each coordsArray as thisCoords, index (thisCoords)}
-      <GlineCode positionsObj={thisCoords} {index} {anim_speed} />
+  <div
+    class="px-10 py-4 bg-white/80 shadow-lg rounded-lg max-h-[100%] overflow-auto"
+  >
+    <h1 class="text-3xl font-extrabold text-slate-400 mb-4">Source Code:</h1>
+    {#each coordsArray as thisCoords, index}
+      <GlineCode positionsObj={thisCoords} {index} />
     {/each}
   </div>
 {/if}
