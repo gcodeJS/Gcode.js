cd3d844de530c2bb77418558e5a77584543627d8
laaouatni <laaouatni.anas@outlook.it>
2022-12-04 12:35:07 +0100

    corrected bug with object if undefined

--- gcode/src/lib/GlineCodeBefore.svelte
+++ gcode/src/lib/GlineCodeBefore.svelte
@@ -4,10 +4,12 @@
 </script>
 
 <div class="flex gap-2 order-first">
-  <span class="text-slate-500">N{index}</span>
-  {#each Object.entries(commandsObj) as command}
-    <span class="text-violet-500 font-bold">
-      {command.join("")}
-    </span>
-  {/each}
+  <span class="text-slate-700">N{index}</span>
+  {#if commandsObj != null}
+    {#each Object.entries(commandsObj) as command}
+      <span class="text-violet-500 font-bold">
+        {command.join("")}
+      </span>
+    {/each}
+  {/if}
 </div>
